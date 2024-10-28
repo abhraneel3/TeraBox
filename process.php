1511c34d90d30d70dc02332aa4c80ecad6c83669
abhraneel3 <nisha.blog.author@gmail.com>
2024-10-28 09:08:11 +0530

    Create process.php

---
+++ process.php
@@ -0,0 +1,6 @@
+<?php
+// process.php
+$videoUrl = $argv[1]; // Get the video URL passed as an argument
+// Simulate getting a value (you can replace this with your logic)
+echo "Video URL: " . $videoUrl . "\n";
+// You can do more processing here...
